e7bacf5a44a40dde6ae30047b4c8c317a6850fc3
KovidNikam <119118490+KovidNikam@users.noreply.github.com>
2024-10-11 10:40:15 +0530

    Update demo1.java

--- demo1.java
+++ demo1.java
@@ -1,6 +1,6 @@
 class demo1{
   public static void main(String args[]){
-    System.out.println();
-    System.out.println();
+    System.out.println("This JAVA program is generated on GITHUB platform.");
+    System.out.println("This JAVA program is excecuted using google app engine.");
   }
 }
